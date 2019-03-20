768573c7ac2cb57b022e7398a1b650871da5d5a8
Silvio Senese <s228789@studenti.polito.it>
2019-03-20 12:27:26 +0100

    Interfaccia grafica completata

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,72 @@
-package it.polito.tdp.spellchecker.controller;
+	package it.polito.tdp.spellchecker.controller;
 
-public class SpellCheckerController {
-	
-}
+	import java.net.URL;
+	import java.util.ResourceBundle;
+	import javafx.event.ActionEvent;
+	import javafx.fxml.FXML;
+	import javafx.scene.control.Button;
+	import javafx.scene.control.ComboBox;
+	import javafx.scene.control.Label;
+	import javafx.scene.control.TextArea;
+	import javafx.scene.control.TextField;
+
+	public class SpellCheckerController {
+
+		//box.getItems().addAll("English","Italian")
+		
+	    @FXML
+	    private ResourceBundle resources;
+
+	    @FXML
+	    private URL location;
+
+	    @FXML
+	    private ComboBox<?> comboBoxLanguage;
+
+	    @FXML
+	    private TextField txtToSpell;
+
+	    @FXML
+	    private Button btnSpellCheck;
+
+	    @FXML
+	    private TextArea txtWrongWords;
+
+	    @FXML
+	    private Label labelErrors;
+
+	    @FXML
+	    private Button btnClearText;
+
+	    @FXML
+	    private Label labelTimeCheck;
+
+	    @FXML
+	    void chooseLanguage(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void doClearText(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void doSpellCheck(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void initialize() {
+	        assert comboBoxLanguage != null : "fx:id=\"comboBoxLanguage\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert txtToSpell != null : "fx:id=\"txtToSpell\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert btnSpellCheck != null : "fx:id=\"btnSpellCheck\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert txtWrongWords != null : "fx:id=\"txtWrongWords\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert labelErrors != null : "fx:id=\"labelErrors\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert btnClearText != null : "fx:id=\"btnClearText\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert labelTimeCheck != null : "fx:id=\"labelTimeCheck\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+
+	    }
+	}
+
+
